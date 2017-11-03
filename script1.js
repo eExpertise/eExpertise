9053faa7d694ef9f79ffe60f4d344e5a67e60815
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-11-03 14:02:21 +0100

    minor update

--- script1.js
+++ script1.js
@@ -1,4 +1,9 @@
 
 $(document).ready(function(){
-	
+
+$('.button').hover(function() {
+$('.button').css({'text-decoration':'underline'});
+}, function() { $('.button').css({'text-decoration':'none'});
+});
+
 });
