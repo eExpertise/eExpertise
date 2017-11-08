63a8d981a8513e59014517f5c6f468261a0d184f
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-11-08 17:00:42 +0100

    Mega update!!
    added lots, fixed lots and thought lots...

---
+++ index-script.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function(){
+	
+$('.header-head').delay(500).fadeIn(500);
+
+$('#link1').hover(function() {
+$('#link1').css({'text-decoration':'underline'});
+}, function() { $('#link1').css({'text-decoration':'none'});
+});
+
+$('#link2').hover(function() {
+$('#link2').css({'text-decoration':'underline'});
+}, function() { $('#link2').css({'text-decoration':'none'});
+});
+
+$('#link3').hover(function() {
+$('#link3').css({'text-decoration':'underline'});
+}, function() { $('#link3').css({'text-decoration':'none'});
+});
+
+$('#link4').hover(function() {
+$('#link4').css({'text-decoration':'underline'});
+}, function() { $('#link4').css({'text-decoration':'none'});
+});
+
+$('#link5').hover(function() {
+$('#link5').css({'text-decoration':'underline'});
+}, function() { $('#link5').css({'text-decoration':'none'});
+});
+
+$('#link6').hover(function() {
+$('#link6').css({'text-decoration':'underline'});
+}, function() { $('#link6').css({'text-decoration':'none'});
+});
+
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 350) {
+    $('#p1').fadeIn(300);
+  } else {
+    
+  }
+});
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 1000) {
+    $('#p2').fadeIn(300);
+  } else {
+
+  }
+});
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 1300) {
+    $('#p3').fadeIn(300);
+  } else {
+
+  }
+});
+
+
+});
