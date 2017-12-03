923f58f5a7734357ac6b98f142cbb7309ec24d0c
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-12-03 17:29:30 +0100

    minor update

--- script2-2.js
+++ script2-2.js
@@ -95,6 +95,36 @@ $(document).ready(function(){
 
     $('.option1').data('clicked', !($(this).data('clicked')))
 	
+	if ($('.option2').data('clicked')) {
+	$('.option2').data('clicked', !($('.option2').data('clicked')));
+	}
+	
+	if ($(".option2").data('clicked')) {
+	$(".option2").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option2").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option3').data('clicked')) {
+	$('.option3').data('clicked', !($('.option3').data('clicked')));
+	}
+	
+	if ($(".option3").data('clicked')) {
+	$(".option3").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option3").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option4').data('clicked')) {
+	$('.option4').data('clicked', !($('.option4').data('clicked')));
+	}
+	
+	if ($(".option4").data('clicked')) {
+	$(".option4").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option4").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
 	if ($(".option1").data('clicked')) {
 	$(this).css({"text-shadow":"none", "color":"#7cb3e9"});
 	} else {
@@ -119,6 +149,36 @@ $(document).ready(function(){
 	$('.option2').click(function() {
    
     $('.option2').data('clicked', !($(this).data('clicked')))
+	
+	if ($('.option1').data('clicked')) {
+	$('.option1').data('clicked', !($('.option1').data('clicked')));
+	}
+	
+	if ($(".option1").data('clicked')) {
+	$(".option1").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option1").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option3').data('clicked')) {
+	$('.option3').data('clicked', !($('.option3').data('clicked')));
+	}
+	
+	if ($(".option3").data('clicked')) {
+	$(".option3").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option3").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option4').data('clicked')) {
+	$('.option4').data('clicked', !($('.option4').data('clicked')));
+	}
+	
+	if ($(".option4").data('clicked')) {
+	$(".option4").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option4").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
 	
 	if ($(".option2").data('clicked')) {
 	$(this).css({"text-shadow":"none", "color":"#7cb3e9"});
@@ -145,6 +205,36 @@ $(document).ready(function(){
    
     $('.option3').data('clicked', !($(this).data('clicked')))
 	
+	if ($('.option1').data('clicked')) {
+	$('.option1').data('clicked', !($('.option1').data('clicked')));
+	}
+	
+	if ($(".option1").data('clicked')) {
+	$(".option1").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option1").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option2').data('clicked')) {
+	$('.option2').data('clicked', !($('.option2').data('clicked')));
+	}
+	
+	if ($(".option2").data('clicked')) {
+	$(".option2").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option2").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option4').data('clicked')) {
+	$('.option4').data('clicked', !($('.option4').data('clicked')));
+	}
+	
+	if ($(".option4").data('clicked')) {
+	$(".option4").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option4").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
 	if ($(".option3").data('clicked')) {
 	$(this).css({"text-shadow":"none", "color":"#7cb3e9"});
 	} else {
@@ -169,6 +259,36 @@ $(document).ready(function(){
 	$('.option4').click(function() {
    
     $('.option4').data('clicked', !($(this).data('clicked')))
+	
+	if ($('.option1').data('clicked')) {
+	$('.option1').data('clicked', !($('.option1').data('clicked')));
+	}
+	
+	if ($(".option1").data('clicked')) {
+	$(".option1").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option1").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option3').data('clicked')) {
+	$('.option3').data('clicked', !($('.option3').data('clicked')));
+	}
+	
+	if ($(".option3").data('clicked')) {
+	$(".option3").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option3").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
+	
+	if ($('.option2').data('clicked')) {
+	$('.option2').data('clicked', !($('.option2').data('clicked')));
+	}
+	
+	if ($(".option2").data('clicked')) {
+	$(".option2").css({"text-shadow":"none", "color":"#7cb3e9"});
+	} else {
+	$(".option2").css({"text-shadow":"2px 2px #2280dd", "color":"white"});
+	}
 	
 	if ($(".option4").data('clicked')) {
 	$(this).css({"text-shadow":"none", "color":"#7cb3e9"});
@@ -195,48 +315,15 @@ $(document).ready(function(){
 	if ($('.option1').data('clicked') && !($('.option2').data('clicked')) && !($('.option3').data('clicked')) && !($('.option4').data('clicked'))) {
 	Cookies.set('options', '1000');
 	alert("1000");
-	} else if ($('.option1').data('clicked') && $('.option2').data('clicked') && !($('.option3').data('clicked')) && !($('.option4').data('clicked'))) {
-	Cookies.set('options', '1100');
-	alert("1100");
-	} else if ($('.option1').data('clicked') && !($('.option2').data('clicked')) && $('.option3').data('clicked') && !($('.option4').data('clicked'))) {
-	Cookies.set('options', '1010');
-	alert("1010");
-	} else if ($('.option1').data('clicked') && !($('.option2').data('clicked')) && !($('.option3').data('clicked')) && $('.option4').data('clicked')) {
-	Cookies.set('options', '1001');
-	alert("1001");
-	} else if ($('.option1').data('clicked') && $('.option2').data('clicked') && $('.option3').data('clicked') && !($('.option4').data('clicked'))) {
-	Cookies.set('options', '1110');
-	alert("1110");
-	} else if ($('.option1').data('clicked') && $('.option2').data('clicked') && !($('.option3').data('clicked')) && $('.option4').data('clicked')) {
-	Cookies.set('options', '1101');
-	alert("1101");
-	} else if ($('.option1').data('clicked') && !($('.option2').data('clicked')) && $('.option3').data('clicked') && $('.option4').data('clicked')) {
-	Cookies.set('options', '1011');
-	alert("1011");
 	} else if (!($('.option1').data('clicked')) && $('.option2').data('clicked') && !($('.option3').data('clicked')) && !($('.option4').data('clicked'))) {
 	Cookies.set('options', '0100');
 	alert("0100");
-	} else if (!($('.option1').data('clicked')) && $('.option2').data('clicked') && $('.option3').data('clicked') && !($('.option4').data('clicked'))) {
-	Cookies.set('options', '0110');
-	alert("0110");
-	} else if (!($('.option1').data('clicked')) && $('.option2').data('clicked') && !($('.option3').data('clicked')) && $('.option4').data('clicked')) {
-	Cookies.set('options', '0101');
-	alert("0101");
-	} else if (!($('.option1').data('clicked')) && $('.option2').data('clicked') && $('.option3').data('clicked') && $('.option4').data('clicked')) {
-	Cookies.set('options', '0111');
-	alert("0111");
 	} else if (!($('.option1').data('clicked')) && !($('.option2').data('clicked')) && $('.option3').data('clicked') && !($('.option4').data('clicked'))) {
 	Cookies.set('options', '0010');
 	alert("0010");
-	} else if (!($('.option1').data('clicked')) && !($('.option2').data('clicked')) && $('.option3').data('clicked') && $('.option4').data('clicked')) {
-	Cookies.set('options', '0011');
-	alert("0011");
 	} else if (!($('.option1').data('clicked')) && !($('.option2').data('clicked')) && !($('.option3').data('clicked')) && $('.option4').data('clicked')) {
 	Cookies.set('options', '0001');
 	alert("0001");
-	} else if ($('.option1').data('clicked') && $('.option2').data('clicked') && $('.option3').data('clicked') && $('.option4').data('clicked')) {
-	Cookies.set('options', '1111');
-	alert("1111");
 	} else {
 	event.preventDefault();
 	alert('ERROR: No options selected. you pick an option by clicking on either HARDWARE, DESIGN, SOFTWARE and/or GAMING.');
