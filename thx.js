ea83a55bb3a6342af07998bfab4ba60d4aad9dd5
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-12-07 09:55:17 +0100

    minor update

--- thx.js
+++ thx.js
@@ -3,36 +3,6 @@ $(document).ready(function(){
 	
 $('.header-head').delay(700).fadeIn(500);
 
-$('#link1').hover(function() {
-$('#link1').css({'text-decoration':'underline'});
-}, function() { $('#link1').css({'text-decoration':'none'});
-});
-
-$('#link2').hover(function() {
-$('#link2').css({'text-decoration':'underline'});
-}, function() { $('#link2').css({'text-decoration':'none'});
-});
-
-$('#link3').hover(function() {
-$('#link3').css({'text-decoration':'underline'});
-}, function() { $('#link3').css({'text-decoration':'none'});
-});
-
-$('#link4').hover(function() {
-$('#link4').css({'text-decoration':'underline'});
-}, function() { $('#link4').css({'text-decoration':'none'});
-});
-
-$('#link5').hover(function() {
-$('#link5').css({'text-decoration':'underline'});
-}, function() { $('#link5').css({'text-decoration':'none'});
-});
-
-$('#link6').hover(function() {
-$('#link6').css({'text-decoration':'underline'});
-}, function() { $('#link6').css({'text-decoration':'none'});
-});
-
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
@@ -47,23 +17,23 @@ $(document).scroll(function() {
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 1020) {
+  if (y > 980) {
     $("#p2").animate({
     opacity: "1"
-    })
+    });
   } else {
-
+    
   }
 });
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 1000) {
+  if (y > 1270) {
     $("#p3").animate({
     opacity: "1"
     });
   } else {
-
+    
   }
 });
 
@@ -72,9 +42,14 @@ $('#logo').css({'text-decoration':'underline'});
 }, function() { $('#logo').css({'text-decoration':'none'});
 });
 
-function openNav() {
-    document.getElementById("side-bar").style.width = "250px";
-}
+$("#menu").click(function(){
+$("#slide-bar").animate({width:"100%"});
+$("#menu2").delay(280).fadeIn(150);
+});
 
+$("#closebtn").click(function() {
+$("#slide-bar").animate({width:"0"});
+$("#menu2").fadeOut(150);
+});
 
 });
