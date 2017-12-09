55ba86dc348815cc16c09b0cec2ddec0b678640d
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-12-09 23:42:38 +0100

    minor update

--- thx.js
+++ thx.js
@@ -2,125 +2,125 @@
 $(document).ready(function(){
 Cookies.get("options");
 Cookies.get("options2");
-if (Cookies.get('options'&& 'options2') == '1000', '1000') {
+if (Cookies.get('options') == '1000' && Cookies.get('options2') == '1000')) {
     alert("1000, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1100', '1000') {
+} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '1000') {
     alert("1100, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1010', '1000') {
+} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '1000') {
     alert("1010, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1001', '1000') {
+} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '1000') {
 	alert("1001, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1110', '1000') {
+} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '1000') {
 	alert("1110, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1101', '1000') {
+} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '1000') {
 	alert("1101, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1011', '1000') {
+} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '1000') {
 	alert("1011, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0100', '1000') {
+} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '1000') {
 	alert("0100, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0110', '1000') {
+} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '1000') {
 	alert("0110, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0101', '1000') {
+} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '1000') {
 	alert("0101, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0111', '1000') {
+} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '1000') {
 	alert("0111, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0010', '1000') {
+} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '1000') {
     alert("0010, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0011', '1000') {
+} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '1000') {
 	alert("0011, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0001', '1000') {
+} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '1000') {
 	alert("0001, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1111', '1000') {
+} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '1000') {
 	alert("1111, 1000");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1000', '0100') {
+} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0100') {
     alert("1000, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1100', '0100') {
+} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0100') {
 	alert("1100, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1010', '0100') {
+} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0100') {
 	alert("1010, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1001', '0100') {
+} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '0100') {
 	alert("1001, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1110', '0100') {
+} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0100') {
 	alert("1110, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1101', '0100') {
+} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0100') {
 	alert("1101, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1011', '0100') {
+} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0100') {
 	alert("1011, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0100', '0100') {
+} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0100') {
 	alert("0100, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0110', '0100') {
+} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0100') {
 	alert("0110, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0101', '0100') {
+} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0100') {
 	alert("0101, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0111', '0100') {
+} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0100') {
 	alert("0111, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0010', '0100') {
+} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0100') {
     alert("0010, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0011', '0100') {
+} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0100') {
 	alert("0011, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0001', '0100') {
+} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0100') {
 	alert("0001, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1111', '0100') {
+} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0100') {
     alert("1111, 0100");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1100', '0010') {
+} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0010') {
 	alert("1100, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1010', '0010') {
+} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0010') {
 	alert("1010, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1001', '0010') {
-	alert("1000, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1110', '0010') {
+} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0010') {
+	alert("1001, 0010");
+} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0010') {
 	alert("1110, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1101', '0010') {
+} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0010') {
 	alert("1101, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1011', '0010') {
+} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0010') {
 	alert("1011, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0100', '0010') {
+} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0010') {
 	alert("0100, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0110', '0010') {
+} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0010') {
 	alert("0110, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0101', '0010') {
+} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0010') {
 	alert("0101, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0111', '0010') {
+} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0010') {
 	alert("0111, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0010', '0010') {
+} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0010') {
     alert("0010, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0011', '0010') {
+} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0010') {
 	alert("0011, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0001', '0010') {
+} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0010') {
 	alert("0001, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1111', '0010') {
+} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0010') {
 	alert("1111, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1000', '0010') {
+} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0010') {
     alert("1000, 0010");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1100', '0001') {
+} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0001') {
 	alert("1100, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1010', '0001') {
+} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0001') {
 	alert("1010, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1001', '0001') {
+} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '0001') {
 	alert("1001, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1110', '0001') {
+} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0001') {
 	alert("1110, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1101', '0001') {
+} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0001') {
 	alert("1101, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1011', '0001') {
+} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0001') {
 	alert("1011, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0100', '0001') {
+} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0001') {
 	alert("0100, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0110', '0001') {
+} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0001') {
 	alert("0110, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0101', '0001') {
+} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0001') {
 	alert("0101, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0111', '0001') {
+} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0001') {
 	alert("0111, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0010', '0001') {
+} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0001') {
     alert("0010, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0011', '0001') {
+} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0001') {
 	alert("0011, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '0001', '0001') {
+} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0001') {
 	alert("0001, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1111', '0001') {
+} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0001') {
     alert("1111, 0001");
-} else if (Cookies.get('options') && Cookies.get('options2') == '1000', '0001') {
+} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0001') {
 	alert("1000, 0001");
 } else {
 alert('ERROR: not all required options were selected, please go back and finish your personalization.')
