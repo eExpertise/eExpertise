2610604baf9d15f26d7499055385c0c747a72d9c
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2017-12-20 07:41:06 +0100

    minor update

--- sample-script.js
+++ sample-script.js
@@ -3,59 +3,59 @@
 		
 		$(document).width();
 		$("#sample1").hover(function() {
-			$("#sample-header1").animate({right:"50%"}, 200);
-			$("#sample1").animate({width:"75%"}, 200);
-			$("#sample2").animate({width:"85%"}, 200);
-			$("#sample3").animate({width:"95%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header1").animate({right:"50%"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"75%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"85%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"95%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		}, function() {
-			$("#sample-header1").animate({right:"0"}, 200);
-			$("#sample1").animate({width:"23%"}, 200);
-			$("#sample2").animate({width:"48%"}, 200);
-			$("#sample3").animate({width:"74%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header1").animate({right:"0"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"23%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"48%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"74%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		});
 		
 		$("#sample2").hover(function() {
-			$("#sample-header2").animate({right:"50%"}, 200);
-			$("#sample1").animate({width:"10%"}, 200);
-			$("#sample2").animate({width:"80%"}, 200);
-			$("#sample3").animate({width:"90%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header2").animate({right:"50%"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"10%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"80%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"90%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		}, function() {
-			$("#sample-header2").animate({right:"0"}, 200);
-			$("#sample1").animate({width:"23%"}, 200);
-			$("#sample2").animate({width:"48%"}, 200);
-			$("#sample3").animate({width:"74%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header2").animate({right:"0"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"23%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"48%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"74%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		});
 		
 		$("#sample3").hover(function() {
-			$("#sample-header3").animate({right:"50%"}, 200);
-			$("#sample1").animate({width:"10%"}, 200);
-			$("#sample2").animate({width:"20%"}, 200);
-			$("#sample3").animate({width:"90%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header3").animate({right:"50%"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"10%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"20%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"90%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		}, function() {
-			$("#sample-header3").animate({right:"0"}, 200);
-			$("#sample1").animate({width:"23%"}, 200);
-			$("#sample2").animate({width:"48%"}, 200);
-			$("#sample3").animate({width:"74%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header3").animate({right:"0"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"23%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"48%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"74%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		});
 		
 		$("#sample4").hover(function() {
-			$("#sample-header4").animate({right:"50%"}, 200);
-			$("#sample1").animate({width:"10%"}, 200);
-			$("#sample2").animate({width:"20%"}, 200);
-			$("#sample3").animate({width:"30%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header4").animate({right:"50%"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"10%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"20%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"30%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		}, function() {
-			$("#sample-header4").animate({right:"0"}, 200);
-			$("#sample1").animate({width:"23%"}, 200);
-			$("#sample2").animate({width:"48%"}, 200);
-			$("#sample3").animate({width:"74%"}, 200);
-			$("#sample4").animate({width:"100%"}, 200);
+			$("#sample-header4").animate({right:"0"}, { duration: 300, queue: false });
+			$("#sample1").animate({width:"23%"}, { duration: 300, queue: false });
+			$("#sample2").animate({width:"48%"}, { duration: 300, queue: false });
+			$("#sample3").animate({width:"74%"}, { duration: 300, queue: false });
+			$("#sample4").animate({width:"100%"}, { duration: 300, queue: false });
 		});
 		
 	});
