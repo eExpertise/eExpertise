e4add24b73e5e24fff557b45c9d311cb48d158bd
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-02-04 17:52:45 +0100

    test

--- js/jQuery/thx.js
+++ js/jQuery/thx.js
@@ -10,186 +10,128 @@
 		});
 		
 		if (Cookies.get('options') == '1000' && Cookies.get('options2') == '1000') {
-			$("#hardware, #beginner").removeClass(".no-interest");
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware And Design";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware And Software";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware And Gaming";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware, Design and Software";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware, Design And Gaming";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware, Software And Gaming";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '1000') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0100') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0010') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1100' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1010' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1001' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1110' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1101' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1011' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0100' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0110' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0101' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0111' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0010' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0011' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '0001' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1111' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else if (Cookies.get('options') == '1000' && Cookies.get('options2') == '0001') {
-			$scope.niche = "Hardware";
-			$scope.experience = "Beginner";
+			$("#hardware, #beginner").removeClass(".not-interest");
 		} else {
-		alert('ERROR: not all required options were selected, please go back and finish your personalization.');
+			alert('ERROR: not all required options were selected, please go back and finish your personalization.');
+			$("#hardware, #beginner").removeClass(".not-interest");
 		}
 		
 		$('#header-head').delay(400).fadeIn(500);
