d2a65e53a1178fe5da6903d552ef86d66e8a76a3
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-02-09 07:38:36 +0100

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 
 app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname, '/../../index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
