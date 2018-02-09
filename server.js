c030039b59de82a74d4c2dea414885532fdb4929
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-02-09 18:43:27 +0100

    updated server.js

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-
 app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, '/../index.html'));
+    res.sendFile(path.join(__dirname, '/../opt.html'));
+	res.sendFile(path.join(__dirname, '/../personalization1.html'));
+	res.sendFile(path.join(__dirname, '/../personalization2.html'));
+	res.sendFile(path.join(__dirname, '/../thank-you.html'));
+	res.sendFile(path.join(__dirname, '/../sample-preview.html'));
 });
 
 app.listen(PORT, () => {
