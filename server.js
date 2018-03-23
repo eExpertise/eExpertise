cefd3d017d9ef7752511f3aed8c926350fb306e0
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-03-23 09:03:37 +0100

    major file management

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database('./db.sqlite')
-var path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
 
-var PORT = process.env.PORT || 8080;
+const { getElementById, getIndexById, updateElement, createElement } = require('./utils');
+
+	const PORT = process.env.PORT || 8080;
 
 
 
