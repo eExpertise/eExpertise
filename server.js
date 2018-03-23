b9850e9cedd62b86b445cc902db1a6685eaa173c
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-03-23 15:53:47 +0100

    added MySQL and removed junk nodeJS code

--- server.js
+++ server.js
@@ -7,17 +7,7 @@ const { getElementById, getIndexById, updateElement, createElement } = require('
 
 	const PORT = process.env.PORT || 8080;
 
-
-
 app.use(express.static('public'));
-
-app.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '/public/opt.html'));
-	res.sendFile(path.join(__dirname, '/public/personalization1.html'));
-	res.sendFile(path.join(__dirname, '/public/personalization2.html'));
-	res.sendFile(path.join(__dirname, '/public/thank-you.html'));
-	res.sendFile(path.join(__dirname, '/public/sample-preview.html'));
-});
 
 app.listen(PORT, () => {
 	console.log(`The server is listening on port ${PORT}`);
