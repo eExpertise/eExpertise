90818339e4ffe7b7f446c9cc4fc51b3d47e0e7d8
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-04-26 18:31:18 +0200

    changed index href

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -13,42 +13,6 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-/*
-passport.use(new GoogleStrategy({
-        // options for google strategy
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        // check if user already exists in our own db
-        User.findOne({googleId: profile.id}).then((currentUser) => {
-            if (currentUser) {
-                // already have this user
-                console.log('user is: ', currentUser);
-                done(null, currentUser);
-            } else {
-                // if not, create user in our db
-                var newUser = new User() 
-                // setting up user settings from google
-                newUser.google.googleId  = profile.id;
-                newUser.google.username  = profile.username;
-                newUser.google.thumbnail = profile.thumbnail;
-                newUser.google.email     = profile.emails[0].value;
-                // save user
-                newUser.save().then((err) => {
-                    if (err) {
-                        throw err; 
-                    }  
-                    console.log('created new user: ', newUser);
-                    done(null, newUser);
-                }).catch(() => {
-                    console.log('promise rejected');
-                });
-            }
-        });
-    })
-); */
-
 passport.use(
     new GoogleStrategy({
         // options for google strategy
